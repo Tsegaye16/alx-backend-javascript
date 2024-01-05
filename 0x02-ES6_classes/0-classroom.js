fdf31f2ce5e35bb87c59e25cd777995c9753adb2
Tsegaye16 <abewatsegaye16@gmail.com>
2024-01-05 01:34:17 +0000

    second day back-end

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
- export default class ClassRoom {
-   constructor(maxStudentSize) {
-    this._maxStudentSize = maxStudentSize;
-   }
- }
+export default class ClassRoom {
+  constructor(maxStudentSize) {
+    this._maxStudentsSize = maxStudentSize;
+  }
+}
