2f8a483ada65039736154985a61b34b802265fd6
Tsegaye16 <abewatsegaye16@gmail.com>
2024-01-05 01:43:09 +0000

    second day back-end

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,6 @@
+import ClassRoom from './1-make_classrooms.js';
+
+export default function initializeRooms() {
+  const arr = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return arr;
+}
