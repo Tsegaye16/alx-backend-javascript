fb6e1348bf338e87fcce5398406be86f1e7a099e
Tsegaye16 <abewatsegaye16@gmail.com>
2024-01-05 02:25:21 +0000

    second day back-end

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,15 +1,12 @@
-export default class Car {
-  constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
   }
 
   static get [Symbol.species]() {
-    return this.prototype.constructor;
-  }
-
-  cloneCar() {
-    return new (this.constructor[Symbol.species])();
+    return Car;
   }
 }
