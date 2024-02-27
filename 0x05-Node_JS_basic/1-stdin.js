23b314471ae36a96db8bfdc5deaa0434547510c1
Tsegaye16 <abewatsegaye16@gmail.com>
2024-02-27 23:58:26 +0000

    first day of node

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,16 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const r1 = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-console.log("Welcome to Holberton School, what is your name?");
-r1.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-}}
-
-r1.on('close', () => {
-  console.log("\nThis important software is now closing");
-})
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
