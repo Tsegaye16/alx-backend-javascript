1b01af44ca3723c4779a5f4e89bfa15210324f56
Tsegaye16 <abewatsegaye16@gmail.com>
2024-02-29 19:06:02 +0000

    alx unit test

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,19 +1,19 @@
 module.exports = function calculateNumber(type, a, b) {
-  const numA = Number(a);
-  const numB = Number(b);
+  const numberA = Number(a);
+  const numberB = Number(b);
 
-  if (Number.isNaN(numA) || Number.isNaN(numB))
+  if (Number.isNaN(numberA) || Number.isNaN(numberB))
   throw TypeError;
 
   if (type === 'SUM') {
-    return (Math.round(numA) + Math.round(numB));
+    return (Math.round(numberA) + Math.round(numberB));
   } else if (type === 'SUBTRACT') {
-    return (Math.round(numA) - Math.round(numB));
+    return (Math.round(numberA) - Math.round(numberB));
   } else if (type === 'DIVIDE') {
-      if (Math.round(numB) === 0) {
+      if (Math.round(numberB) === 0) {
         return ('Error');
       }
-      return (Math.round(numA) / Math.round(numB));
+      return (Math.round(numberA) / Math.round(numberB));
   } else {
     throw TypeError;
   }
